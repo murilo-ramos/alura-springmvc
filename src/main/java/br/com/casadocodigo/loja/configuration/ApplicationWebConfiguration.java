3bafa191793369ba046e15c5ea1d1f2bc419468a
Murilo Costa <murilo.ramosc@outlook.com>
2017-09-25 22:42:46 -0300

    Add validation messages, refactor post url for form

--- src/main/java/br/com/casadocodigo/loja/configuration/ApplicationWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/configuration/ApplicationWebConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.casadocodigo.loja.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -18,6 +20,17 @@ public class ApplicationWebConfiguration {
         resolve.setPrefix("/WEB-INF/views/");
         resolve.setSuffix(".jsp");
         return resolve;
+    }
+	
+	@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("/WEB-INF/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(1);
+
+        return messageSource;
     }
 	
 }
