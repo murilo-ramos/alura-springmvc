764effefcdda1e8690036605bba1fdeb53ca6eaa
Murilo Costa <murilo.ramosc@outlook.com>
2018-01-23 23:52:22 -0200

    Adding log4j configuration, disabling file saver exception and change it to just a log, in order to prevent server issues (for learning)

--- src/main/java/br/com/casadocodigo/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/infra/FileSaver.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileSaver {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileSaver.class);
 	
 	@Autowired
 	private HttpServletRequest httpRequest;
@@ -18,11 +21,12 @@ public class FileSaver {
 	public String saveFileToDisk(String baseFolder, MultipartFile file) {
 		String path = baseFolder + "/" + file.getOriginalFilename();		
 		String realPath = httpRequest.getServletContext().getRealPath("/"+path);
-		
+				
 		try {
 			file.transferTo(new File(realPath));
 		} catch (IllegalStateException | IOException ex) {
-			throw new RuntimeException(ex.getMessage());
+			//throw new RuntimeException(ex.getMessage());
+			LOGGER.error("Error when saving file to disk: " + ex.getMessage());
 		}
 		
 		return path;
