3d122f6858dd8f67ed57f648be11d36f285c56de
Murilo Costa <murilo.ramosc@outlook.com>
2018-03-22 21:52:22 -0300

    Add Configuration annotation to JPA productino configuration

--- src/main/java/br/com/casadocodigo/loja/configuration/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/configuration/JPAProductionConfiguration.java
@@ -8,11 +8,13 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Profile("Prod")
+@Configuration
 public class JPAProductionConfiguration {
 	
 	@Autowired
